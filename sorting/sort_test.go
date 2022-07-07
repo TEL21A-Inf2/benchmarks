c20b4e9af47061dabe2fcf82e526c18729fd0f35
Reiner Hüchting <dev@reinerhuechting.de>
2022-07-07 22:32:57 +0200

    Testhelfer in die Testdatei verschoben.

--- sorting/sort_test.go
+++ sorting/sort_test.go
@@ -47,10 +47,20 @@ func TestHeapSort(t *testing.T) {
 	}
 }
 
+// Hilfsfunktion: Liefert die Test-Listen für die Tests.
 func testData() [][]int {
 	l1 := []int{5, 235, 2, 3, 42, 1, 107, 7, 12}
 	l2 := []int{1, 2, 3, 4, 5}
 	l3 := []int{}
 
 	return [][]int{l1, l2, l3}
+}
+
+// Hilfsfunktion: Wendet eine Funktion auf die Liste an, prüft,
+// ob die Liste anschließend sortiert ist und löst ggf. den Fehler aus.
+func testSortAlgorithm(sortFunc func([]int), list []int, name string, t *testing.T) {
+	sortFunc(list)
+	if !sort.IntsAreSorted(list) {
+		t.Errorf("%s: %v sollte sortiert sein, ist es aber nicht.", name, list)
+	}
 }
